9dc87ca9a5b3a67c052658fd2c51ad02f33b6600
2daw20 <msantosgarcia.guadalupe@alumnado.fundacionloyola.net>
2021-05-19 12:14:50 +0200

    new version

---
+++ v2/scriptsSQL/maquinas.sql
@@ -0,0 +1,7 @@
+USE appJesuita;
+
+INSERT INTO Maquina (ip,nombreAlumno,nombre,idJesuita,idLugar,tipo,idUsuario,password) VALUES
+    (0,'admin','admin','','','a','0','123456'),
+    (1,'Alumno1','alumno1','0','1','u','1','qwer'),
+    (2,'alumno2','alumno2','1','2','u','2','asdf'),
+    (3,'alumno3','alumno3','2','3','u','3','zxcv');
