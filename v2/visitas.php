f21b82dd4f7b47916d196f159feccdac66a3b978
2daw20 <msantosgarcia.guadalupe@alumnado.fundacionloyola.net>
2021-05-26 13:01:58 +0200

    new version

--- v2/visitas.php
+++ v2/visitas.php
@@ -1,3 +1,48 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Realizar visitas</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    require_once 'clasephp.php';
+    $objeto=new clasephp();
+    $sql1="SELECT * FROM jesuita";
+    $objeto->realizarConsultas($sql1);
+    ?>
+    <div id="general">
+        <label>Jesuita</label>
+        <select name="nJesuita">
+            <?php
+            while($fila=$objeto->extraerFilas()) {
+                echo '<option value="'.$fila['idJesuita'].'">'.$fila['nombre'].'</option>';
+            }
+            ?>
+        </select>
 
-?>
+        <label>Lugares</label>
+        <select name="nLugares">
+            <<?php
+            $sql2="SELECT * FROM Lugar";
+            $objeto->realizarConsultas($sql2);
+            while($fila=$objeto->extraerFilas()) {
+                echo '<option value="'.$fila['idLugar'].'">'.$fila['nombre'].'</option>';
+            }
+            ?>
+        </select>
+    </div>
+    <?php
+    $idJesuita=$_POST['nJesuita'];
+    $idLugar=$_POST['nLugar'];
+    if($idJesuita != NULL && $idLugar != NULL){//si los valores son distintos a null entra en el bucle
+        $sql3="INSERT INTO visita(idJesuita,idLugar) VALUES ('".$idJesuita.",".$idLugar."')";//consulta de insercion de datos
+        $objeto->realizarConsultas($sql3);//consulta de insercion de datos
+        if($idJeusita =1){
+            header("location:listUsers.php");//si hay un usuario vuelve a la pagina anterior
+        }
+    }
+    ?>
+</body>
+</html>
